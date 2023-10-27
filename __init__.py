440f94c24597ec3e8c8d536f65d401a200bd8175
Noxin <kelmcf.scott@gmail.com>
2023-10-26 17:54:28 -0700

    added scaled resolution node with inbuilt scaling

--- __init__.py
+++ __init__.py
@@ -1,1 +1,18 @@
 from .noxin_chimenode import *
+from .noxin_scaledresolution import *
+
+
+
+
+# A dictionary that contains all nodes you want to export with their names
+# NOTE: names should be globally unique
+NODE_CLASS_MAPPINGS = {
+    "NOXCHIME": NOXCHIME,
+    "NOXSCALEDRES": NOXSCALEDRES
+}
+
+# A dictionary that contains the friendly/humanly readable titles for the nodes
+NODE_DISPLAY_NAME_MAPPINGS = {
+    "NOXCHIME": "Noxin Complete Chime",
+    "NOXSCALEDRES": "Noxin Scaled Resolutions"
+}
